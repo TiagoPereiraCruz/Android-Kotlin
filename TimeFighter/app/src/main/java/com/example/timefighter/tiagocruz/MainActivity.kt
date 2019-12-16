331893bcea5469997c10533c9ee4ca7847cd66f2
TiagoPereiraCruz <tiago.peng@gmail.com>
2019-12-15 21:49:08 -0300

    Starting and Ending the game

--- TimeFighter/app/src/main/java/com/example/timefighter/tiagocruz/MainActivity.kt
+++ TimeFighter/app/src/main/java/com/example/timefighter/tiagocruz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,7 +51,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                endGame()
             }
         }
 
@@ -70,5 +71,10 @@ class MainActivity : AppCompatActivity() {
     private fun startGame() {
         countDownTimer.start()
         gameStarted = true
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
+        resetGame()
     }
 }
